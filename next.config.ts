a7cc56e82206b6c59254a85057c5742f88ecc36e
Ubaid Hussain <ubaid7625@gmail.com>
2025-05-14 12:44:36 +0500

    Add SVGR support in webpack configuration and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config) {
+    // Add SVGR configuration
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
