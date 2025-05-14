e9a5b7329b07119945c8887d2767c2fd75c07abc
Ubaid Hussain <ubaid7625@gmail.com>
2025-05-14 14:06:07 +0500

    change footer icons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,6 +5,14 @@ import Link from "next/link";
 import { cn } from "@/utils/cn";
 import Container from "@/components/ui/container";
 import Image from "@/components/ui/image";
+/**
+ * Icons
+ */
+import FacebookIcon from "@public/assets/icons/facebook-circle-filled.svg";
+import InstagramIcon from "@public/assets/icons/instagram.svg";
+import XIcon from "@public/assets/icons/x.svg";
+import LinkedInIcon from "@public/assets/icons/linkedin-square-filled.svg";
+import YoutubeIcon from "@public/assets/icons/youtube-square-filled.svg";
 
 interface FooterProps {
   className?: string;
@@ -75,19 +83,7 @@ const Footer: React.FC<FooterProps> = ({ className }) => {
                 aria-label="Facebook"
               >
                 <div className="w-6 h-6 flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-                  </svg>
+                  <FacebookIcon className="w-6 h-6" />
                 </div>
               </a>
               <a
@@ -97,28 +93,7 @@ const Footer: React.FC<FooterProps> = ({ className }) => {
                 aria-label="Instagram"
               >
                 <div className="w-6 h-6 flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <rect
-                      x="2"
-                      y="2"
-                      width="20"
-                      height="20"
-                      rx="5"
-                      ry="5"
-                    ></rect>
-                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-                    <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-                  </svg>
+                  <InstagramIcon className="w-6 h-6" />
                 </div>
               </a>
               <a
@@ -128,19 +103,7 @@ const Footer: React.FC<FooterProps> = ({ className }) => {
                 aria-label="Twitter"
               >
                 <div className="w-6 h-6 flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
-                  </svg>
+                  <XIcon className="w-6 h-6" />
                 </div>
               </a>
               <a
@@ -150,21 +113,7 @@ const Footer: React.FC<FooterProps> = ({ className }) => {
                 aria-label="LinkedIn"
               >
                 <div className="w-6 h-6 flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-                    <rect x="2" y="9" width="4" height="12"></rect>
-                    <circle cx="4" cy="4" r="2"></circle>
-                  </svg>
+                  <LinkedInIcon className="w-6 h-6" />
                 </div>
               </a>
               <a
@@ -174,20 +123,7 @@ const Footer: React.FC<FooterProps> = ({ className }) => {
                 aria-label="YouTube"
               >
                 <div className="w-6 h-6 flex items-center justify-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"></path>
-                    <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"></polygon>
-                  </svg>
+                  <YoutubeIcon className="w-6 h-6" />
                 </div>
               </a>
             </div>
